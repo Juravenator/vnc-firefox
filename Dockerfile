90f8c52d3aa19dd2f5de6303eadca720a77ec76c
Glenn Dirkx <glenn.dirkx@cern.ch>
2020-03-08 16:27:38 +0100

    fix for ff 52 and up

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:14.04
+FROM ubuntu:18.04
 
 MAINTAINER Juravenator <glenn.dirkx@gmail.com>
 
--- vnc.sh
+++ vnc.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
 
 # Remove VNC lock (if process already killed)
 rm -f /tmp/.X1-lock /tmp/.X11-unix/X1
@@ -8,6 +9,8 @@ cat hosts >> /etc/hosts
 
 # start vnc server
 vncserver :1 -geometry $RESOLUTION -depth $DEPTH
+export DISPLAY=:1
+startlxde &
 
 # start firefox
 /firefox/firefox --display=':1'
